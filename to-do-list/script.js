84933161eb827e362abdf8f5acd1b1f0bf15322f
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-05 19:50:01 +0000

    Adds a to-do list with item addition

---
+++ to-do-list/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();   
+        $('.list').append("<div class='item'>" + toAdd + "</div>");
+    });
+});
