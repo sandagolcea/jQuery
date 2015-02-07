5a113ca392150746194fada0341ff9024b3b36a4
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-07 23:44:55 +0000

    Playing around with mario: on key down

--- events/script.js
+++ events/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('div').hover(
+  $('.info-tabs').hover(
     function(){
         $(this).addClass('active');
     },
@@ -8,5 +8,36 @@ $(document).ready(function(){
         $(this).removeClass('active');
     }
   );
+
+  // input box focus:
+  $('input').focus(function(){
+      $(this).css('outline-color','#FF0000');
+  });
+
+  // animate yellow box on pressing any key
+  $(document).keydown(function(){
+        $('#yellow-box').animate({left:'+=10px'},500);
+  });
+
+  $(document).keydown(function(key) {
+      switch(parseInt(key.which,10)) {
+      // Left arrow key pressed
+      case 37:
+        $('#mario').animate({left: "-=10px"}, 'fast');
+        break;
+      // Up Arrow Pressed
+      case 38:
+        $('#mario').animate({top: "-=10px"}, 'fast');
+        break;
+      // Right Arrow Pressed
+      case 39:
+        $('#mario').animate({left:'+=10px'},500);
+        break;
+      // Down Arrow Pressed
+      case 40:
+        $('#mario').animate({top: "+=10px"}, 'fast');
+        break;
+    }
+  });
 
 });
